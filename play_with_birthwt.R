a3b9652cdf349604fb88b831718996ed2c2d9357
Mehdi Belfegas <mehdi.belfegas@gmail.com>
2017-10-25 00:52:02 +0200

    Add a new baby plot with horizontal lines

--- play_with_birthwt.R
+++ play_with_birthwt.R
@@ -14,7 +14,7 @@ plot(birthwt$age,
      xlab = "Age (years)", 
      ylab = "Weight (kg)",
      xlim = c(10, 50),
-     ylim = c(0, 180))
+     ylim = c(0, 160))
 points(birthwt$age[birthwt$smoke == 1],
        motherWeightKg[birthwt$smoke == 1],
        col = "blue",
@@ -24,7 +24,26 @@ points(birthwt$age,
        col = "green",
        pch = 19)
 legend(x = 40,
-       y = 180,
+       y = 160,
        legend = c("NS mum", "S mum", "baby"),
        col = c("red", "blue", "green"),
        pch = 19)
+x11()
+
+plot(birthwt$age,
+     babyWeightKg,
+     col = "red",
+     pch = 19,
+     main = "baby weight vs Mother age",
+     xlab = "Age (years)", 
+     ylab = "Weight (kg)",
+     xlim = c(10, 50),
+     ylim = c(0, 7))
+legend(x = 45,
+       y = 7,
+       legend = c("Baby"),
+       col = c("red"),
+       pch = 19)
+abline(h = 1, col = "blue", lwd = 2)
+abline(h = 2, col = "green", lwd = 2)
+abline(h = 3, col = "purple", lwd = 2)
