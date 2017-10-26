e1eee111e522a153b0afa18ca523d575ca510ffe
Mehdi Belfegas <mehdi.belfegas@gmail.com>
2017-10-26 18:05:30 +0200

    Test more advanced features using boxplot

--- play_with_birthwt.R
+++ play_with_birthwt.R
@@ -1,7 +1,7 @@
 require(MASS)
 data(birthwt)
 View(birthwt)
-?birthwt
+#?birthwt  # Look for help
 
 motherWeightKg <- birthwt$lwt / 2.2
 babyWeightKg <- birthwt$bwt / 1000
@@ -52,3 +52,50 @@ X11()
 boxplot(babyWeightKg~birthwt$smoke,
         col = c("red", "blue"),
         names = c("Non smoking", "Smoking"))
+
+x11()
+par(mfrow=c(2,1))
+hist(birthwt$age[birthwt$smoke==0],
+     col = grey(0.7),
+     breaks = seq(10, 45, by=5))
+hist(birthwt$age[birthwt$smoke==1],
+     col = grey(0.7),
+     breaks = seq(10, 45, by=5))
+
+barplot(table(birthwt$smoke) / sum(table(birthwt$smoke)),
+        main = "Smoking status",
+        col = c("lightblue", grey(0.8)),
+        name = c("Non smoking", "Smoking"))
+
+pie(table(birthwt$race))
+
+quants <- quantile(birthwt$age, probs=c(0.25, 0.50, 0.75), na.rm = T)
+birthwt_c <-cut(birthwt$age, # Transform a quantitative variable into a qualitative variable
+         c(0, 19, 23, 26, 50),
+         include.lowest = TRUE)
+
+X11()
+boxplot(birthwt$bwt~birthwt_c,
+        col = c("red", "blue", "grey", "purple"))
+
+graphics.off()
+
+boxplot(birthwt$bwt[birthwt$smoke == 0]~birthwt_c[birthwt$smoke == 0],
+        col = "lightblue",
+        at = c(1, 4, 7, 10),
+        xlim = c(0, 12),
+        names=rep("", 4))
+boxplot(birthwt$bwt[birthwt$smoke == 1]~birthwt_c[birthwt$smoke == 1],
+        col = "pink",
+        add=T,
+        at=c(2, 5, 8, 11),
+        names=rep("", 4))
+mtext("[14;19]", 1, line=2, at=1.5)
+mtext("[19;23]", 1, line=2, at=4.5)
+mtext("[23;26]", 1, line=2, at=7.5)
+mtext("[26;50]", 1, line=2, at=10.5)
+legend(1, 5000, 
+       legend=c("Non smoking", "Smoking"),
+       col=c("lightblue", "pink"),
+       fill=c("lightblue", "pink"),
+       horiz = T)
